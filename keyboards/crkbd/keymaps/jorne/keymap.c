0a06772b1b024669571e89308a1945d79869d832
Alexey Ismailov <aismailov@ya.ru>
2021-07-28 00:38:35 +0300

    layout upd

--- keyboards/crkbd/keymaps/jorne/keymap.c
+++ keyboards/crkbd/keymaps/jorne/keymap.c
@@ -156,9 +156,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_RAISE] = LAYOUT(\
-    _______, KC_NLCK, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,     KC_VOLU, KC_HOME, KC_INS,   KC_PGUP, KC_SLCK, KC_CAPS, _______, \
-            EQL_LCT, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_UP,    KC_RGHT, KC_PSCR,  APP_RCT, \
-            _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      KC_VOLD, KC_END,  KC_DOWN,  KC_PGDN, KC_PAUS, _______, \
+    _______, KC_NLCK, KC_PSLS, KC_7,   KC_8,   KC_9,   KC_MINS,     KC_VOLU, KC_HOME, KC_INS,   KC_PGUP, KC_SLCK, KC_CAPS, _______, \
+            EQL_LCT, KC_PAST, KC_4,   KC_5,   KC_6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_UP,    KC_RGHT, KC_PSCR,  APP_RCT, \
+            _______, KC_0,   KC_1,   KC_2,   KC_3,   KC_PCMM,      KC_VOLD, KC_END,  KC_DOWN,  KC_PGDN, KC_PAUS, _______, \
                                         _______, _______, _______,     _______, _______, _______\
     ),
 
