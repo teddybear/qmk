b397d1df1373084ec0b23c8d52efdfcf405962f8
Alexey Ismailov <aismailov@ya.ru>
2021-08-06 00:23:11 +0300

    layout align

--- keyboards/crkbd/keymaps/jorne/keymap.c
+++ keyboards/crkbd/keymaps/jorne/keymap.c
@@ -142,53 +142,61 @@ enum custom_keycodes { QWERTY = SAFE_RANGE, LOWER, RAISE, ADJUST, BACKLIT, RGBRS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(\
-    ESC_LGUI, KC_GRV,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,       KC_O,    KC_P,    KC_LBRC, RGUI_T(KC_RBRC), \
-              KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,       KC_L,    KC_SCLN, RCTL_T(KC_QUOT), \
-              KC_LALT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH, BSL_RAL, \
-                                          TAB_RSE, SPC_LSH, ENT_LWR,   ENT_LWR, BSP_RSH, DEL_RSE \
+    ESC_LGUI, KC_GRV,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,             KC_Y,    KC_U,    KC_I,       KC_O,    KC_P,    KC_LBRC, RGUI_T(KC_RBRC), \
+              KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,             KC_H,    KC_J,    KC_K,       KC_L,    KC_SCLN, RCTL_T(KC_QUOT), \
+              KC_LALT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,             KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH, BSL_RAL, \
+                                        TAB_RSE, SPC_LSH, ENT_LWR,             ENT_LWR, BSP_RSH, DEL_RSE \
     ),
 
     [_LOWER] = LAYOUT(\
-    _______, KC_UNDS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  F12_RGU, \
-            PLS_LCT, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, MIN_RCT, \
-            EQL_LAL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-                                        _______, _______, _______,     _______, _______, _______\
+    _______,  KC_UNDS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,            KC_F6,   KC_F7,   KC_F8,      KC_F9,   KC_F10,  KC_F11,  F12_RGU, \
+              PLS_LCT,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR,    KC_LPRN, KC_RPRN, MIN_RCT, \
+              EQL_LAL,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,             KC_6,    KC_7,    KC_8,       KC_9,    KC_0,    _______, \
+                                          _______, _______,  _______,          _______, _______, _______\
     ),
 
     [_RAISE] = LAYOUT(\
-    _______, KC_NLCK, KC_PSLS, KC_7,   KC_8,   KC_9,   KC_MINS,     KC_VOLU, KC_HOME, KC_INS,   KC_PGUP, KC_SLCK, KC_CAPS, _______, \
-            EQL_LCT, KC_PAST, KC_4,   KC_5,   KC_6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_UP,    KC_RGHT, KC_PSCR,  APP_RCT, \
-            _______, KC_0,   KC_1,   KC_2,   KC_3,   KC_PCMM,      KC_VOLD, KC_END,  KC_DOWN,  KC_PGDN, KC_PAUS, _______, \
-                                        _______, _______, _______,     _______, _______, _______\
+    _______,  KC_NLCK,  KC_PSLS, KC_7,    KC_8,    KC_9,     KC_MINS,          KC_VOLU, KC_HOME, KC_INS,     KC_PGUP, KC_SLCK, KC_CAPS, _______, \
+              EQL_LCT,  KC_PAST, KC_4,    KC_5,    KC_6,     KC_PPLS,          KC_MUTE, KC_LEFT, KC_UP,      KC_RGHT, KC_PSCR, APP_RCT, \
+              _______,  KC_0,    KC_1,    KC_2,    KC_3,     KC_PCMM,          KC_VOLD, KC_END,  KC_DOWN,    KC_PGDN, KC_PAUS, _______, \
+                                          _______, _______,  _______,          _______, _______, _______\
     ),
 
     [_ADJUST] = LAYOUT(\
-    RESET,  CMB_ON,  KC_ASUP, KC_BTN1, KC_BTN3, KC_BTN2, PLOVER,       PLOVER, QWERTY,  KC_ASDN, KC_ASTG, KC_ASUP, CMB_ON,   RESET, \
-            CMB_TOG, RGB_HUI, KC_MS_L, KC_MS_U, KC_MS_R, KC_WH_U,      KC_LEFT,  KC_DOWN,  KC_UP, KC_RGHT, RGB_HUI, CMB_TOG, \
-            CMB_OFF, TG_FIVE, RGB_SAD, KC_MS_D, TG_ISO,  KC_WH_D,      TG_THMB, TG_ISO,  RGB_VAD, RGB_SAD, TG_FIVE, CMB_OFF, \
-                                        _______, SH_TG,   _______,     _______, SH_TG,   _______\
+    RESET,  CMB_ON,     KC_ASUP, KC_BTN1, KC_BTN3, KC_BTN2,  XKBMAP,           _______, QWERTY,   KC_ASDN,    KC_ASTG, KC_ASUP, CMB_ON,   RESET, \
+            CMB_TOG,    RGB_HUI, KC_MS_L, KC_MS_U, KC_MS_R,  KC_WH_U,          KC_LEFT, KC_DOWN,  KC_UP,      KC_RGHT, RGB_HUI, CMB_TOG, \
+            CMB_OFF,    TG_FIVE, RGB_SAD, KC_MS_D, TG_ISO,   KC_WH_D,          TG_THMB, TG_ISO,   RGB_VAD,    RGB_SAD, TG_FIVE, CMB_OFF, \
+                                           _______, SH_TG,   _______,          _______, SH_TG,   _______\
     ),
 
 
     [_THUMB_ALT] = LAYOUT(\
-    _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, \
-            _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, \
-            _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, \
-                                        DEL_RSE, BSP_RSH, ENT_LCPS,      ENT_LWR, SPC_LSH, TAB_RSE \
+    _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______, _______, \
+             _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______, \
+             _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, _______, \
+                                       DEL_RSE, BSP_RSH, ENT_LCPS,             ENT_LWR, SPC_LSH, TAB_RSE \
     ),
 
     [_ISO] = LAYOUT(\
-    _______, _______,         _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______,        _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, \
             LCTL_T(KC_NUBS), _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, \
             LALT_T(KC_NUHS), _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, KC_RALT, \
-                                                _______, _______, _______,      _______, _______, _______\
+                                               _______, _______, _______,      _______, _______, _______\
     ),
 
+/*
+    // [_FIVE] = LAYOUT(\
+    // _______, _______,         _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, \
+    //         LCTL_T(KC_NUBS), _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, \
+    //         LALT_T(KC_NUHS), _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, KC_RALT, \
+    //                                             _______, KC_SPC, _______,      _______, KC_SPC, _______\
+    // ),
+*/
     [_FIVE] = LAYOUT(\
-    _______, _______,         _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, \
-            LCTL_T(KC_NUBS), _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, \
-            LALT_T(KC_NUHS), _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, KC_RALT, \
-                                                _______, KC_SPC, _______,      _______, KC_SPC, _______\
+    RESET,   RGBRST,  KC_ASUP, KC_ASTG, KC_ASDN, _______, _______,             _______, _______,  KC_ASDN, KC_ASTG, KC_ASUP, RGBRST,   RESET, \
+             RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, QWERTY,  _______,             _______,  QWERTY,  RGB_VAI, RGB_SAI, RGB_HUI, RGB_TOG, \
+             RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, TG_FIVE,  TG_THMB,            TG_THMB, TG_ISO,   RGB_VAD, RGB_SAD, RGB_HUD, RGB_MOD, \
+                                         _______, SH_TG,   _______,            _______, SH_TG,   _______\
     ),
 
 };
