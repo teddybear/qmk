c37166c45174092098ae04dfec2eda8bbfed9575
Alexey Ismailov <aismailov@ya.ru>
2021-08-17 02:22:15 +0300

    jorne36_upd

--- keyboards/crkbd/keymaps/jorne_36/keymap.c
+++ keyboards/crkbd/keymaps/jorne_36/keymap.c
@@ -72,12 +72,12 @@ enum custom_keycodes { QWERTY = SAFE_RANGE, SYM, SYMA, NUM, NAV, FUN, MOUSE};
 #define RC_J RCTL_T(KC_J)
 #define GR_DOT RALT_T(KC_DOT)
 
-#define ESC_NUM LT(_NUM, KC_ESC)
+#define ESC_FUN LT(_FUN, KC_ESC)
 #define SPC_NAV LT(_NAV, KC_SPC)
 #define TAB_MOS LT(_MOUSE, KC_TAB)
 #define ENT_SYM LT(_SYM, KC_ENT)
 #define BSP_NUM LT(_NUM, KC_BSPC)
-#define DEL_FUN LT(_FUN, KC_DEL)
+#define DEL_NAV LT(_NAV, KC_DEL)
 
 #include "taps.c"
 #include "hld.c"
@@ -89,27 +89,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                       KC_Y,    KC_U,    KC_I,       KC_O,    KC_P,  \
             LG_A,    LA_S,    LS_D,    LC_F,     KC_G,                       KC_H,    RC_J,    RS_K,       RA_L,    RG_SC,  \
             KC_Z,    GR_X,    KC_C,    KC_V,     KC_B,                       KC_N,    KC_M,    KC_COMM,    GR_DOT,  KC_SLSH, \
-                                      ESC_NUM,  SPC_NAV, TAB_MOS,           ENT_SYM, BSP_NUM, DEL_FUN \
+                                      ESC_FUN,  SPC_NAV, TAB_MOS,           ENT_SYM, BSP_NUM, DEL_NAV \
+    ),
+
+    [_SYM] = LAYOUT(\
+            KC_LCBR,   KC_AMPR,  KC_ASTR,    KC_LPRN,   KC_RPRN,                    _______,  _______,   _______, _______, _______, \
+            KC_COLN,   KC_DLR,   KC_PERC,    KC_CIRC,   KC_PPLS,                     _______,  _______,   _______, _______, _______,  \
+            KC_TILD,   KC_EXLM,  KC_AT,      KC_HASH,   KC_PIPE,                    _______,  _______,   _______, _______, _______,   \
+                                    KC_LPRN,    KC_RPRN,   KC_UNDS,          _______, _______, _______\
+    ),
+
+    [_NUM] = LAYOUT(\
+            KC_LBRC,   KC_7,    KC_8,    KC_9,   KC_RBRC,                    _______,  _______,   _______, _______, _______, \
+            KC_SCLN,   KC_4,    KC_5,    KC_6,   KC_EQL,                     _______,  _______,   _______, _______, _______,  \
+            KC_GRV,    KC_1,    KC_2,    KC_3,   KC_BSLS,                    _______,  _______,   _______, _______, _______,   \
+                                    _______,    KC_0,   KC_MINS,          _______, _______, _______\
+    ),
+
+    [_NAV] = LAYOUT(\
+            _______,  _______,   _______, _______, _______,                  _______,   KC_HOME,   KC_INS,  KC_PGUP,   KC_DQT,       \
+            _______,  _______,   _______, _______, _______,                  KC_CAPS,   KC_LEFT,   KC_UP,   KC_RGHT,   KC_QUOT,        \
+            _______,  _______,   _______, _______, _______,                  _______,   KC_END,    KC_DOWN, KC_PGDN,   KC_BSLS,       \
+                                    _______, _______, _______,            _______, _______,  _______ \
+    ),
+
+    [_FUN] = LAYOUT(\
+            KC_F12,   KC_F7,    KC_F8,    KC_F9,   KC_PSCR,                 _______,  _______,   _______, _______, _______, \
+            KC_F11,   KC_F4,    KC_F5,    KC_F6,   KC_SLCK,                 _______,  _______,   _______, _______, _______,  \
+            KC_F10,   KC_F1,    KC_F2,    KC_F3,   KC_PAUS,                 _______,  _______,   _______, _______, _______,   \
+                                    _______,    _______,   _______,       _______, _______, KC_APP\
+    ),
+
+    [_MOUSE] = LAYOUT(\
+            _______,  _______,   _______, _______, _______,                  KC_BTN1,   KC_WH_R,   KC_INS,  KC_WH_U,  _______,       \
+            _______,  _______,   _______, _______, _______,                  KC_BTN3,   KC_MS_L,   KC_MS_U, KC_MS_R,  _______,        \
+            _______,  _______,   _______, _______, _______,                  KC_BTN2,   KC_WH_L,   KC_MS_D, KC_WH_D,  _______,       \
+                                    _______, _______, _______,            _______, _______,  _______ \
     ),
 
     [_SYMA] = LAYOUT(\
             KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,                     KC_F7,   KC_F8,      KC_F9,   KC_F10,  KC_F11, \
-            KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,                   KC_CIRC, KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN,\
-            KC_1,    KC_2,    KC_3,     KC_4,     KC_5,                      KC_6,    KC_7,       KC_8,    KC_9,    KC_0,   \
+            KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,   KC_PERC,                   KC_CIRC, KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN, \
+            KC_1,    KC_2,    KC_3,     KC_4,     KC_5,                      KC_6,    KC_7,       KC_8,    KC_9,    KC_0,     \
                                     _______, _______,  _______,          _______, _______, _______\
-    ),
-
-    [_SYM] = LAYOUT(\
-            KC_LBRC,   KC_7,    KC_8,    KC_9,   KC_RBRC,                     _______,  _______,   _______, _______, _______, \
-            KC_SCLN,   KC_4,    KC_5,    KC_6,   KC_EQL,                      _______,  _______,   _______, _______, _______,\
-            KC_GRV,    KC_1,    KC_2,    KC_3,   KC_BSLS,                     _______,  _______,   _______, _______, _______, \
-                                    _______, KC_0,  KC_GRV,          _______, _______, _______\
     )
 
 };
 
 
-uint32_t layer_state_set_user(uint32_t state) { return update_tri_layer_state(state, _SYM, _NAV, _MOUSE); }
+// uint32_t layer_state_set_user(uint32_t state) { return update_tri_layer_state(state, _SYM, _NAV, _MOUSE); }
 
 void persistent_default_layer_set(uint16_t default_layer) {
     eeconfig_update_default_layer(default_layer);
