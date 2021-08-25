43218c88d30258d3f4545a65f04127bb83df3cb2
Alexey Ismailov <aismailov@ya.ru>
2021-08-25 22:14:05 +0300

    jian layout

--- keyboards/crkbd/keymaps/jorne_corne/keymap.c
+++ keyboards/crkbd/keymaps/jorne_corne/keymap.c
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(\
             LGUI_T(KC_GRV),    KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,             KC_Y,    KC_U,    KC_I,       KC_O,    KC_P,            RGUI_T(KC_LBRC),  \
             KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,     KC_G,             KC_H,    KC_J,    KC_K,       KC_L,    KC_SCLN,         RCTL_T(KC_QUOT), \
-            KC_LALT,           KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,             KC_N,    KC_M,    KC_COMM,    KC_DOT,  RALT_T(KC_SLSH), BSH_LAL, \
+            KC_LALT,           KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,             KC_N,    KC_M,    KC_COMM,    KC_DOT,  KC_SLSH, BSL_RAL, \
                                                  TAB_RSE, SPC_LSH, ESC_LWR,           ENT_LWR, BSP_RSH, DEL_RSE \
     ),
 
---
+++ keyboards/jian/keymaps/teddy/rules.mk
@@ -0,0 +1,5 @@
+SWAP_HANDS_ENABLE = yes
+EXTRAKEY_ENABLE = yes    # Audio control and System control
+#TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
